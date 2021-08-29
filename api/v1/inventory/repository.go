ddd42acc108ba571542c35489f426fd4c123846e
lewis <lxx0103@yahoo.com>
2021-08-29 15:56:32 +0800

    Fix: init

--- api/v1/inventory/repository.go
+++ api/v1/inventory/repository.go
@@ -35,10 +35,10 @@ func (r *inventoryRepository) GetItemByID(id int64) (Item, error) {
 func (r *inventoryRepository) GetItemCount(filter ItemFilter) (int, error) {
 	where, args := []string{"1 = 1"}, []interface{}{}
 	if v := filter.SKU; v != "" {
-		where, args = append(where, "code like ?"), append(args, "%"+v+"%")
+		where, args = append(where, "sku like ?"), append(args, "%"+v+"%")
 	}
 	if v := filter.Name; v != "" {
-		where, args = append(where, "location like ?"), append(args, "%"+v+"%")
+		where, args = append(where, "name like ?"), append(args, "%"+v+"%")
 	}
 	var count int
 	err := r.conn.Get(&count, `
@@ -54,10 +54,10 @@ func (r *inventoryRepository) GetItemCount(filter ItemFilter) (int, error) {
 func (r *inventoryRepository) GetItemList(filter ItemFilter) ([]Item, error) {
 	where, args := []string{"1 = 1"}, []interface{}{}
 	if v := filter.SKU; v != "" {
-		where, args = append(where, "code like ?"), append(args, "%"+v+"%")
+		where, args = append(where, "sku like ?"), append(args, "%"+v+"%")
 	}
 	if v := filter.Name; v != "" {
-		where, args = append(where, "location like ?"), append(args, "%"+v+"%")
+		where, args = append(where, "name like ?"), append(args, "%"+v+"%")
 	}
 	args = append(args, filter.PageId*filter.PageSize-filter.PageSize)
 	args = append(args, filter.PageSize)
